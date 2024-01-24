e951908415e6cfd98831c05f58e2d724d1158a5c
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2024-01-24 14:15:30 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Homepage</h1>
+      <p>this is where the homepage will be dudes</p>
+      <p>I hope you like my project</p>
+    </div>
+  );
+}
+
+export default App;
