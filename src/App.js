85295d85e49fb8900366158240d5aa153c8f7db0
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2024-01-26 13:08:27 -0700

    Added Image folder and main components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-      <p>this is where the homepage will be dudes</p>
-      <p>I hope you like my project</p>
-    </div>
+    <React className="App">
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </React>
   );
 }
 
