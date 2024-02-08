34f35b9473c0372743f8c83f735c71331c3efbf7
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2024-02-08 15:24:37 -0700

    Update CSS for Main

--- src/Main.js
+++ src/Main.js
@@ -8,62 +8,67 @@ import adrian from "./Images/Mario and Adrian b.jpg";
 function Main() {
   return (
     <main class="main-container">
-      <div class="cards">
-        <article class="card">
-          <img src={salad} alt="close up on a Greek Salad" />
-          <header>
-            <h2>Greek Salad</h2>
-          </header>
-          <div class="content">
-            <p>Try our delicious Greek Salad</p>
-          </div>
-          <footer> Order Online! </footer>
-        </article>
-        <article class="card">
-          <img src={bruchetta} alt="Bruchetta topped baguette" />
-          <header>
-            <h2>Bruchetta</h2>
-          </header>
-          <div class="content">
-            <p>
-              Our Bruchetta is a great addition to any dish or salad. Topped
-              with fresh tomatoes
-            </p>
-          </div>
-          <footer>Order Online!</footer>
-        </article>
-        <article class="card">
-          <img src={dessert} alt="a slice of lemon cake " />
-          <header>
-            <h2>Lemon Dessert</h2>
-          </header>
-          <div class="content">
-            <p>Our lemon dessert is made with fresh meyer lemons!</p>
-          </div>
-          <footer>Order Online!</footer>
-        </article>
+      <div class="menu">
+        <div class="cards">
+          <article class="card">
+            <img src={salad} alt="close up on a Greek Salad" />
+            <header>
+              <h2>Greek Salad</h2>
+              <p>$12.99</p>
+            </header>
+            <div class="content">
+              <p>Try our delicious Greek Salad</p>
+            </div>
+            <footer> Order Online! </footer>
+          </article>
+          <article class="card">
+            <img src={bruchetta} alt="Bruchetta topped baguette" />
+            <header>
+              <h2>Bruchetta</h2>
+              <p>$5.99</p>
+            </header>
+            <div class="content">
+              <p>Our Bruchetta is a great addition to any dish or salad.</p>
+            </div>
+            <footer>Order Online!</footer>
+          </article>
+          <article class="card">
+            <img src={dessert} alt="a slice of lemon cake " />
+            <header>
+              <h2>Lemon Dessert</h2>
+              <p>$7.99</p>
+            </header>
+            <div class="content">
+              <p>Our lemon dessert is made with fresh meyer lemons!</p>
+            </div>
+            <footer>Order Online!</footer>
+          </article>
+        </div>
       </div>
       <div class="testimonials">
-        <h2>Testimonials</h2>
+        <h2 class="testimonial-headline">Testimonials</h2>
         <div class="reviews">
-          <h3>5 stars</h3>
-          <p>
-            "I love Little Lemon. Especially their Bruchetta, its great! - Tracy{" "}
-          </p>
-        </div>
-        <div class="reviews">
-          <h3>5 stars</h3>
-          <p>
-            "I had my birthday at Little Lemon. They provided the best Lemon
-            Cake ever! - Rebecca{" "}
-          </p>
-        </div>
-        <div class="reviews">
-          <h3>5 stars</h3>
-          <p>
-            "Little Lemon is a great weekend night out. They have live music on
-            Fridays!{" "}
-          </p>
+          <div>
+            <h3>5 stars</h3>
+            <p>
+              "I love Little Lemon. Especially their Bruchetta, its great! -
+              Tracy{" "}
+            </p>
+          </div>
+          <div>
+            <h3>5 stars</h3>
+            <p>
+              "I had my birthday at Little Lemon. They provided the best Lemon
+              Cake ever! - Rebecca{" "}
+            </p>
+          </div>
+          <div>
+            <h3>5 stars</h3>
+            <p>
+              "Little Lemon is a great weekend night out. They have live music
+              on Fridays!{" "}
+            </p>
+          </div>
         </div>
       </div>
       <div>
