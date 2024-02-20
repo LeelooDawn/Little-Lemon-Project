91dde70e13e205cb076969ce2998c645c8613693
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2024-02-20 14:41:31 -0700

    Added simple booking pages

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Hero/Header";
+import Testimonials from "./components/Testimonials/Testimonials";
+import About from "./components/About/About";
+
+function BookingPage() {
+  return (
+    <>
+      <Header />
+      <h1>This is where the Booking will go</h1>
+      <Testimonials />
+      <About />
+    </>
+  );
+}
+
+export default BookingPage;
