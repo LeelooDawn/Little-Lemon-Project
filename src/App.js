9606a2b36b33337099153de67584f60e211b4778
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2024-02-20 14:31:03 -0700

    Update app js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react.router.dom";
-import "./App.css";
 import Nav from "./components/NavBar/Nav";
 import Homepage from "./pages/Homepage";
 import Footer from "./Footer";
