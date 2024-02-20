75a43ce2aea7a089694ae936b5be7706a8d1bbb8
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2024-02-20 14:48:15 -0700

    Update Booking forms

---
+++ src/components/BookingForms/BookingConfirmation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BookingConfirmation() {
+  return (
+    <div className="container">
+      <h1> Here is your Confirmation </h1>
+    </div>
+  );
+}
+
+export default BookingConfirmation;
---
+++ src/components/BookingForms/BookingForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BookingForm() {
+  return (
+    <div className="container">
+      <h1> Here is your form for booking </h1>
+    </div>
+  );
+}
+
+export default BookingForm;
